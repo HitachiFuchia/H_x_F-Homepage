3329983042d05d9b9b501ecbf134c8b45f0a391b
Hitachi_Fuchia <108301527+HitachiFuchia@users.noreply.github.com>
2023-11-13 23:22:16 +0100

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,13 @@ const header = document.querySelector(".nav__container");
 const menu = document.querySelector(".header__menu");
 const overlay = document.querySelector(".header__overlay");
 const body = document.querySelector("body");
+const CTA = document.querySelector(".nav__container--heading__button");
+
+CTA.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(e);
+  window.location.href = "#contact";
+})
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("open");
