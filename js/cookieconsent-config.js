06eb11918ddb049f7a3a6d1e2ebf76b7a44fbc66
Hitachi_Fuchia <108301527+HitachiFuchia@users.noreply.github.com>
2023-12-15 00:39:14 +0100

    Add files via upload

---
+++ js/cookieconsent-config.js
@@ -0,0 +1,85 @@
+import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@v3.0.0-rc.17/dist/cookieconsent.umd.js';
+
+// Enable dark mode
+document.documentElement.classList.add('cc--darkmode');
+
+CookieConsent.run({
+    guiOptions: {
+        consentModal: {
+            layout: "bar",
+            position: "bottom",
+            equalWeightButtons: false,
+            flipButtons: false
+        },
+        preferencesModal: {
+            layout: "box",
+            position: "left",
+            equalWeightButtons: true,
+            flipButtons: false
+        }
+    },
+    categories: {
+        necessary: {
+            readOnly: true
+        },
+        functionality: {},
+        analytics: {},
+        marketing: {}
+    },
+    language: {
+        default: "de",
+        autoDetect: "browser",
+        translations: {
+            de: {
+                consentModal: {
+                    title: "Hallo Besucher, es ist Kekszeit 🍪",
+                    description: "Hallo, diese Website verwendet essentielle Cookies, um ihren ordnungsgemäßen Betrieb zu gewährleisten, und Tracking-Cookies, um zu verstehen, wie Sie mit ihr interagieren. Letztere werden nur nach Zustimmung gesetzt werden.",
+                    acceptAllBtn: "Alle akzeptieren",
+                    acceptNecessaryBtn: "Alle ablehnen",
+                    showPreferencesBtn: "Einstellungen verwalten",
+                    footer: "<a href=\"https://hitachifuchia.netlify.app/Datenschutzerklaerung.html\">Datenschutz</a>\n<a href=\"https://hitachifuchia.netlify.app/Datenschutzerklaerung.html\">Bedingungen und Konditionen</a>",
+                    closeIconLabel: "Alle ablehnen und schließen"
+                },
+                preferencesModal: {
+                    title: "Präferenzen für die Zustimmung",
+                    acceptAllBtn: "Alle akzeptieren",
+                    acceptNecessaryBtn: "Alle ablehnen",
+                    savePreferencesBtn: "Einstellungen speichern",
+                    closeIconLabel: "Modal schließen",
+                    serviceCounterLabel: "Dienstleistungen",
+                    sections: [
+                        {
+                            title: "Verwendung von Cookies",
+                            description: "Ich verwende Cookies, um die grundlegenden Funktionen der Website zu gewährleisten und um Ihr Online-Erlebnis zu verbessern. Sie können für jede Kategorie wählen, ob Sie sich ein- oder austragen möchten. Für weitere Einzelheiten zu Cookies und anderen sensiblen Daten lesen Sie bitte die vollständige Datenschutzerklärung."
+                        },
+                        {
+                            title: "Streng Notwendige Cookies <span class=\"pm__badge\">Immer Aktiviert</span>",
+                            description: "Diese Cookies sind für das ordnungsgemäße Funktionieren meiner Website unerlässlich. Ohne diese Cookies würde die Website nicht richtig funktionieren.",
+                            linkedCategory: "necessary"
+                        },
+                        {
+                            title: "Funktionalitäts Cookies",
+                            description: "Diese Cookies sind für das ordnungsgemäße Funktionieren meiner Website unerlässlich. Ohne diese Cookies würde die Website nicht richtig funktionieren.",
+                            linkedCategory: "functionality"
+                        },
+                        {
+                            title: "Analytische Cookies",
+                            description: "Diese Cookies ermöglichen es der Website, sich an die von Ihnen in der Vergangenheit getroffenen Auswahl zu erinnern.",
+                            linkedCategory: "analytics"
+                        },
+                        {
+                            title: "Werbung Cookies",
+                            description: "Diese Cookies sammeln informationen darüber, wie Sie die Website nutzen, welche Seiten sie besucht und welche Links sie angeklickt haben. Alle Daten sind anonymisiert und können nicht verwendet werden, um Sie zu identifizieren.",
+                            linkedCategory: "marketing"
+                        },
+                        {
+                            title: "Weitere Informationen",
+                            description: "Wenn Sie Fragen zu meiner Politik in Bezug auf Cookies und Ihre Wahlmöglichkeiten haben, wenden Sie sich bitte an <a class=\"henryfrank98@hotmail.com\" href=\"https://hitachifuchia.netlify.app/html/impressum\">Kontaktiere Sie mich</a>."
+                        }
+                    ]
+                }
+            }
+        }
+    },
+    disablePageInteraction: true
+});
